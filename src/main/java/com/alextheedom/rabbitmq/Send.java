718b0d828d3a3efeed5e070cd96bb9590d6fe09d
Alex Theedom <alex@indigococo.com>
2015-11-21 11:10:50 +0000

    Basic send/receive rabbit example

---
+++ src/main/java/com/alextheedom/rabbitmq/Send.java
@@ -0,0 +1,30 @@
+package com.alextheedom.rabbitmq;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+/**
+ * Created by atheedom on 21/11/15.
+ */
+public class Send {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws java.io.IOException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.99.100");
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        factory.setPort(5692);
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello World!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+}
